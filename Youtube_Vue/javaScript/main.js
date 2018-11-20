51979c2c894f15a7737f776c78e77ba3930d087a
Unknown <dumendlovu@gmail.com>
2018-11-20 09:46:46 +0200

    API get test

--- Youtube_Vue/javaScript/main.js
+++ Youtube_Vue/javaScript/main.js
@@ -1,7 +1,7 @@
 var app =new Vue({
   el:'#videoTab',
   data:{
-    results:[],
+    results: [],
     template: 'it works',
     videoSRC: 'original'
   },
@@ -9,10 +9,12 @@ var app =new Vue({
     var APIkey ='';
     var plLiID ='PL-IVpXTWXdQ2VLSyJaWOkpGA8jR_7nbu0';
 
-    //console.log(this.results);
+    console.log(this.results);
     axios.get('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId='+plLiID+'&key='+APIkey+'').then(function (response){
       this.results = response.data;
-      //console.log(response.data);
+      console.log(this.results);
+      this.videoSRC = this.results.items[0].snippet.resourceId.videoId;
+      console.log(this.videoSRC);
     });
   }
 });
